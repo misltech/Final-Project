405ee2b2dba203b7a3149b55accaa3b435058350
misltech <misltech@DESKTOP-FQNRTVU>
2017-02-23 14:55:04 -0500

    Add project files.

---
+++ TestComplete/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Pi_Quartos
+{
+    public sealed partial class MainPage : Page
+    {
+        Sensor sensor1 = new Sensor(5);
+        LED led1 = new LED(6);
+       
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            if (sensor1.active())
+            {
+                sensor1.Start();
+            }
+
+            //qmrservice.qmrserviceSoapClient client = new qmrservice.qmrserviceSoapClient();
+
+            //client.AddRoomAsync("testroom");
+
+            //qmrservice.GetListRoomsResponse g = await client.GetListRoomsAsync();
+            //Motion();
+            Test();
+        }
+
+        private async void Test()
+        {
+            qmrservice.qmrserviceSoapClient client = new qmrservice.qmrserviceSoapClient();
+
+            qmrservice.GetListRoomsResponse g = await client.GetListRoomsAsync();
+
+            
+        }
+        //public void Motion()
+        //{
+
+        //    sensor1.Detected();
+        //    if (motionDetected == true)
+        //    {
+        //        led1.Led_On();
+        //    }
+        //    else
+        //        led1.Led_Off();
+        //}
+ 
+    }
+}
---
+++ TestComplete/Sensor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace Pi_Quartos
+{
+    class Sensor
+    {
+        //few important datatypes
+        private int _SENSOR_PIN;
+        private GpioPin _pin;
+        private GpioPinValue _pinValue;
+        //private LED _led1 = new LED(6);
+        //private LED _led2 = new LED(21);
+        private bool _sensorstatus;
+        private Boolean _occupied;
+        //qmrservice.qmrserviceSoap client = new qmrservice.qmrserviceSoap();
+        //qmrservice.qmrserviceSoapClient tes = new qmrservice.qmrserviceSoapClient();
+        
+            
+            
+            //Constructor 
+        public Sensor(int SensorPin)
+        {
+            var gpio = GpioController.GetDefault();
+            _SENSOR_PIN = SensorPin;
+            _pin = gpio.OpenPin(_SENSOR_PIN);
+            _pin.SetDriveMode(GpioPinDriveMode.Input);
+
+        }
+        //Check to see if sensor port is giving out a reading
+        public bool active()
+        {
+            if (_pin == null)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+        //Starts listening for changes in sensor
+        public void Start()
+        {
+            _pin.DebounceTimeout = TimeSpan.FromSeconds(1);
+            _pin.ValueChanged += SensorValueChanged;
+          
+        }
+
+
+        //Handles LED light on movement
+        private void SensorValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            _pinValue = _pin.Read();
+            if (_pinValue == GpioPinValue.High)
+            {
+                //_led2.Led_On();
+                //check current state
+                if (!_occupied)
+                {
+                    //write to database that the room is reserved
+                    _occupied = true;
+                }
+                else
+                {
+                    //_led1.Led_Off();
+                    //if (_occupied)
+                    //{
+                    //    //write to database that room is not active
+                    //    _occupied = false;
+                    //}
+                }
+
+            }
+        }
+    }
+}
+// **************************************BETA May or May not work***********************************************
+
+//if (args.Edge == GpioPinEdge.RisingEdge)
+//{
+//    _pinValue = _pin.Read();
+//    _motion = true;
+//   // _led1.Led_On();
+//}
+//else if (args.Edge == GpioPinEdge.FallingEdge)
+//{
+//    _pinValue = _pin.Read();
+//    _motion = false;
+// _led1.Led_Off();
